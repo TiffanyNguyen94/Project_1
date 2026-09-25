1e48722db690f2871fa335a32113740132a32bc5
Tiffany Nguyen <tnguyen94@student.cscc.edu>
2026-09-25 01:35:47 -0400

    created bread selection

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from ingredients import breads, meats, cheeses, toppings
 
 order = []
-print
+
+
+#Bread selection
+
+print("\nChoose your bread:\n")
+for number, bread in bread.items():
+    print(f"{number}. {bread['name']} - ${bread['price']:.2f} - {bread['weight']}g")
+
+while True:
+    bread_choice = input("\nEnter your choice: ")
+    if bread_choice in breads:
+        selected_bread = breads[bread_choice]
+
+        order.append({"name": selected_bread["name"],"quantity": 1, "price": selected_bread["price"], "weight": selected_bread["weight"]})
+        print(f"\nYou selected: {selected_bread['name']}.")
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
+
+#Meat selection
